44cc363193f325a919b7474f7eaf5bb19c39ed2b
watch99 <brutal99@gmail.com>
2021-02-02 16:27:09 +0700

    Create New.go

---
+++ api/coresans/New.go
@@ -0,0 +1,10 @@
+package coresans
+
+import (
+	"github.com/SERV4BIZ/gfp/jsons"
+)
+
+// New is begin HScaleDB object same Factory
+func New(jsoConfigHost *jsons.JSONObject) (*CORESAN, error) {
+	return Factory(jsoConfigHost)
+}
